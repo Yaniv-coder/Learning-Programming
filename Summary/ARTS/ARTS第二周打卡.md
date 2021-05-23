d77c63661904378d9ddac18543f03ede57f2a0ae
Yaniv-coder <670188946@qq.com>
2021-05-24 00:01:38 +0800

    add optimization edition & ARTS

--- Summary/ARTS/ARTS第二周打卡.md
+++ Summary/ARTS/ARTS第二周打卡.md
@@ -9,20 +9,45 @@
 
 ## Algorithm
 
-[]()
+[26. 删除有序数组中的重复项](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/)
 
 
 
-优化版本：
+优化版本，即双指针法：
 
 ```java
+public static int optimizationRemoveDuplicates(int[] nums) {
+    final int n = nums.length;
+    // 指向最后一个已排好的元素
+    int j = 0;
+    // i指针遍历数组，以找到与j指针所指元素不同的元素
+    for (int i = 0; i < n; i++) {
+        if (nums[j] != nums[i]) {
+            nums[++j] = nums[i];
+        }
+    }
 
+    return j + 1;
+}
 ```
 
-暴力求解版本：
+利用Set去重特性的版本：
 
 ```java
+public static int removeDuplicates(int[] nums) {
+    // 用来判断元素是否已经出现过
+    final Set<Integer> lastIntSet = new TreeSet<>();
 
+    for (final int num : nums) {
+        lastIntSet.add(num);
+    }
+    final Object[] array = lastIntSet.toArray();
+    for (int i = 0; i < array.length; i++) {
+        nums[i] = (int) array[i];
+    }
+
+    return array.length;
+}
 ```
 
 
--- study.test/src/main/java/com/yan/study/leetcode/medium/TestArrayExercises.java
+++ study.test/src/main/java/com/yan/study/leetcode/medium/TestArrayExercises.java
@@ -17,11 +17,12 @@ public class TestArrayExercises {
     public static void main(String[] args) {
 //        System.out.println("lengthOfLongestSubstring: " + lengthOfLongestSubstring("abcbcvfbs"));
 //        System.out.println("forceLengthOfLongestSubstring: " + forceLengthOfLongestSubstring("abcbcvfbs"));
-        System.out.println("removeDuplicates: " + removeDuplicates(new int[] {0,0,1,1,1,2,2,3,3,4}));
+//        System.out.println("removeDuplicates: " + removeDuplicates(new int[] {0,0,1,1,1,2,2,3,3,4}));
+        System.out.println("optimizationRemoveDuplicates: " + optimizationRemoveDuplicates(new int[] {0,0,1,1,1,2,2,3,3,4}));
     }
 
     /**
-     * 最优解 3.无重复字符的最长子串.
+     * 3.无重复字符的最长子串（最优解 ）.
      * @param s 给定字符串
      * @return 最长子串长度
      */
@@ -85,7 +86,7 @@ public class TestArrayExercises {
     }
 
     /**
-     * 删除排序数组中的重复项.
+     * 26.删除排序数组中的重复项.
      *
      * 题目：给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
      * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
@@ -101,18 +102,43 @@ public class TestArrayExercises {
     public static int removeDuplicates(int[] nums) {
         // 用来判断元素是否已经出现过
         final Set<Integer> lastIntSet = new TreeSet<>();
-        // 存放第一次出现的元素的下标
-        final List<Integer> removeArrIndexList = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            final int num = nums[i];
+        for (final int num : nums) {
             lastIntSet.add(num);
         }
-        final Object[] array = (Object[]) lastIntSet.toArray();
+        final Object[] array = lastIntSet.toArray();
         for (int i = 0; i < array.length; i++) {
             nums[i] = (int) array[i];
         }
 
         return array.length;
+    }
+
+    /**
+     * 26.删除排序数组中的重复项（双指针法，最优解）.
+     *
+     * 题目：给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     *
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2gy9m/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param nums 一个有序数组 nums
+     * @return 删除重复元素后数组的新长度
+     */
+    public static int optimizationRemoveDuplicates(int[] nums) {
+        final int n = nums.length;
+        // 指向最后一个已排好的元素
+        int j = 0;
+        // i指针遍历数组，以找到与j指针所指元素不同的元素
+        for (int i = 0; i < n; i++) {
+            if (nums[j] != nums[i]) {
+                nums[++j] = nums[i];
+            }
+        }
+
+        return j + 1;
     }
 }
